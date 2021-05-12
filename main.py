659d658263bd377894c195f073af3738127003c6
Soogle44 <hsgru3@gmail.com>
2021-05-12 17:49:42 +0900

    add select date

--- main.py
+++ main.py
@@ -3,38 +3,51 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 
-DATABASE_URL = 'postgresql://uobmebyzrrghgx:2f7245be13f63c9885c843551a27ea1d6ffa5dbb7b043df593ac0910d8455b61@ec2-18-233-83-165.compute-1.amazonaws.com:5432/davgkjq1u2a64q'
-engine = create_engine(DATABASE_URL)
-
-df = pd.read_sql(sql='SELECT * FROM data;', con=engine)
-df.to_csv("data.csv")
-
-shops = []
-for shop in df.columns[1::2]:
-    shops.append(shop.replace("man_", ""))
 
 st.title("Oriental Lounge Tracker")
 link = '[Oriental Lounge](https://oriental-lounge.com/)'
 st.markdown(link, unsafe_allow_html=True)
 
-show_shop_list = st.multiselect('select area', shops)
-# show_date_list = st.multiselect('select date')
+try:
+    DATABASE_URL = 'postgresql://uobmebyzrrghgx:2f7245be13f63c9885c843551a27ea1d6ffa5dbb7b043df593ac0910d8455b61@ec2-18-233-83-165.compute-1.amazonaws.com:5432/davgkjq1u2a64q'
+    engine = create_engine(DATABASE_URL)
 
-show_list = []
-for shop in show_shop_list:
-    show_list.append("man_" + shop)
-    show_list.append("woman_" + shop)
+    df = pd.read_sql(sql='SELECT * FROM data;', con=engine)
 
-if show_list != []:
+    shops = []
+    for shop in df.columns[1::2]:
+        shops.append(shop.replace("man_", ""))
+
+    show_shop_list = st.multiselect('select area(s)', shops)
+    show_date = st.date_input("input date")
+
+    show_list = []
+    for shop in show_shop_list:
+        show_list.append("man_" + shop)
+        show_list.append("woman_" + shop)
+
+    for i, date in enumerate(df["date"]):
+        if show_date.day == date:
+            show_df = df.iloc[i:i + 36, :]
+            show_df = show_df[show_list.append("date")]
+            break
+
+    print(show_df)
+
+    # fig = go.Figure(go.Scatter(X=show_df["date"], y=show_df[show_list]))
+
+    # if show_list != []:
 
     fig = go.Figure()
     for col in show_list:
-        fig.add_trace(go.Scatter(x=df["date"], y=df[col], name=col))
+        fig.add_trace(go.Scatter(x=show_df["date"], y=show_df[col], name=col))
 
     fig.update_layout(legend={"x": 0, "y": -0.2, "yanchor": "top"})
     fig.update_layout(margin={"l": 0, "r": 0, "t": 0, "b": 0})
     st.plotly_chart(fig)
 
+    # else:
+    #     st.info("select area(s)")
 
-else:
-    st.info("select area")
+except Exception:
+    st.info("Sorry there are too many connections...")
